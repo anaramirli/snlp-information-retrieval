bae79e7dc1e18afe86039d3b3e066c8277ac1281
uds-groupstatr19 <57177811+uds-groupstatr19@users.noreply.github.com>
2020-08-19 12:38:01 +0200

    successfully debugged bm25 calculation. mean of the precisions via BM25 is 0.1059 which is slightly better than the baseline 0.097.

--- bm25_reranker.py
+++ bm25_reranker.py
@@ -1,47 +1,43 @@
 from baseline_doc_retrieval import *
 from rank_bm25 import BM25Okapi
 
-# Preprocess documents
-articles = IRModel('data\\trec_documents.xml')
-tokenized_documents = articles.documents     # lists of tokenized documents -> [['a', 'malaysian', 'english',], ...]
 
 # Preprocess queries
-tokenized_queries = list()  # list of tokenized queries -> [['what', 'does', 'the', 'peugeot', 'company', 'manufacture'], ...]
+articles = IRModel('data\\trec_documents.xml')
 queries = articles.extract_queries("data\\test_questions.txt") # list of queries as strings
+tokenized_queries = list()  # list of tokenized queries -> [['what', 'does', 'the', 'peugeot', 'company', 'manufacture'], ...]
 for q in queries:
     tokenized_queries.append(articles.preprocess_str(q))
 
 # Extract answers to all queries
 answers = articles.extract_answers("data\\patterns.txt")    # list of lists containing answers to the queries -> [["Young"], ["405", "automobiles?", "diesel\s+motors?" ],...]
 
-# Use baseline model and return the top 1000 documents given a query
+# Use baseline model and get the top 1000 documents for each query
 baseline_top_1000 = list()   # top 1000 documents for all queries -> [['a malaysian english', '...'], ...]
+tokenized_baseline_1000 = list()
 for q in queries:
     scores = articles.similarity_scores(q)[:1000]  # top 1000 documents for each query -> [(document number, score),..]
     documents = articles.find_document(scores)  # Get document content by document number -> [['a', 'malaysian', 'english',], ...]
+    tokenized_baseline_1000.append(documents)
     docs = list()
     for doc in documents:
         docs.append(' '.join(doc))
     baseline_top_1000.append(docs)
-
 print(" Top 1000 from Baseline are prepared....")
 
-# Calculate scores via bm25
-bm25 = BM25Okapi(baseline_top_1000)
-# top 50 documents based on the top 1000 documents returned in (a) for all queries
+# Use BM25 to get top 50 documents based on the top 1000 documents returned from baseline
 bm25_top_50 = list()    # list of lists with ranked documents as strings -> [['It is quite windy in London', '...'], ...]
 tokenized_bm25_top_50 = list()
-for q, docs in zip(tokenized_queries, baseline_top_1000):
-    bm25 = BM25Okapi(docs)
+for q, docs, tokenized_docs in zip(tokenized_queries, baseline_top_1000, tokenized_baseline_1000):
+    bm25 = BM25Okapi(tokenized_docs)
     top_50 = bm25.get_top_n(q, docs, n=50)
     bm25_top_50.append(top_50)
     tokens = list()
     for doc in top_50:
         tokens.append(doc.split())
     tokenized_bm25_top_50.append(tokens)
-    print(tokens)
 
-# Mean of the precisions
-print('Calculating mean of the precisions...')
-print('\nprecision mean:', articles.precisions_mean(queries, answers, tokenized_bm25_top_50)) # precision mean: 0.019
+# BM25: Mean of the precisions
+print('Calculating mean of the precisions (BM25)...')
+print('\nprecision mean (BM25):', articles.precisions_mean(queries, answers, tokenized_bm25_top_50)) # precision mean:  0.1059
 
